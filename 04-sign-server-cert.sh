c971ff5d4a10e9956bc79f4166a6053816a3f841
Sofyan Sugianto <sofyanartem@gmail.com>
2023-10-04 10:18:47 +0700

    Create 04-sign-server-cert.sh
    sign csr from step 03, will generate server.crt

---
+++ 04-sign-server-cert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CONFIG=$1
+WORKDIR=$2
+CADIR=$3
+
+help()
+{
+	echo "Usage: $0 serverconfigfile serverdirectory cadirectory"
+        echo "Ensure that config file is exists, see example-server.conf"
+        echo "Ensure that serverdirectory is exists"
+        echo "Ensure your ca directory is valid"
+	exit 10
+}
+
+[[ -z $WORKDIR || -z $CONFIG || -z $CADIR ]] && help
+[[ ! -e "$CONFIG" ]] && help 
+[[ ! -e "$CADIR/ca.key" || ! -e "$CADIR/ca.crt" || ! -e "$CADIR/serial" ]] && help 
+
+openssl x509 -req -days 365 -in "$WORKDIR/server.csr" -extfile "$CONFIG" -extensions v3_req -CA "$CADIR/ca.crt" -CAkey "$CADIR/ca.key" -CAserial "$CADIR/serial" -CAcreateserial -out "$WORKDIR/server.crt"
