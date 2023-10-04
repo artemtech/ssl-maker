ccfdcd7a5e2d1b6bb1f953ef05743ed79eccfe05
Sofyan Sugianto <sofyanartem@gmail.com>
2023-10-04 10:32:06 +0700

    fix wrong serial path and set notext when generate ca.crt

--- 02-create-rootca.sh
+++ 02-create-rootca.sh
@@ -16,11 +16,11 @@ help()
 [[ -e "$WORKDIR/ca.crt" ]] && help 
 
 mkdir -p $WORKDIR/{private,crl,newcerts}
-touch $WORKDIR/{index.txt, serial}
+touch $WORKDIR/{index.txt,serial}
 echo "1000" > $WORKDIR/serial
 
 openssl req -verbose -config "$CONFIG" -new -key "$WORKDIR/ca.key" -sha512 -out "$WORKDIR/ca.csr"
 
-openssl ca -config "$CONFIG" -out "$WORKDIR/ca.crt" -keyfile "$WORKDIR/ca.key" -verbose -selfsign -md sha512 -infiles "$WORKDIR/ca.csr"
+openssl ca -config "$CONFIG" -notext -out "$WORKDIR/ca.crt" -keyfile "$WORKDIR/ca.key" -verbose -selfsign -md sha512 -infiles "$WORKDIR/ca.csr"
 
 #openssl req -x509 -new -config "$CONFIG" -days 3650 -nodes -key "$WORKDIR/ca.key" -sha512 -verbose -out "$WORKDIR/ca.crt"
