493c84c5d7970956a8a6a2c0969e52f612af6e68
Sofyan Sugianto <sofyanartem@gmail.com>
2023-11-17 17:28:43 +0700

    fix wrong ca path result

--- 02-create-rootca.sh
+++ 02-create-rootca.sh
@@ -19,5 +19,5 @@ mkdir -p $WORKDIR/{private,crl,newcerts}
 touch $WORKDIR/{index.txt,serial}
 echo "1000" > $WORKDIR/serial
 
-openssl req -verbose -config "$CONFIG" -new -x509 -extensions v3_ca -sha512 -key "$WORKDIR/ca.key" -out "$WORKDIR/ca.csr"
+openssl req -verbose -config "$CONFIG" -new -x509 -extensions v3_ca -sha512 -key "$WORKDIR/ca.key" -out "$WORKDIR/ca.crt"
 
