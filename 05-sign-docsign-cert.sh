d01018f35231206a0c4186866a5f6e4e1de8d672
Sofyan Sugianto <sofyanartem@gmail.com>
2023-11-17 17:58:53 +0700

    update docsign script docs

--- 05-sign-docsign-cert.sh
+++ 05-sign-docsign-cert.sh
@@ -6,9 +6,9 @@ CADIR=$3
 
 help()
 {
-	echo "Usage: $0 serverconfigfile serverdirectory cadirectory"
-        echo "Ensure that config file is exists, see example-server.conf"
-        echo "Ensure that serverdirectory is exists"
+	echo "Usage: $0 docsignconfigfile docsigndirectory cadirectory"
+        echo "Ensure that config file is exists, see example-docsign.conf"
+        echo "Ensure that docsigndirectory is exists"
         echo "Ensure your ca directory is valid"
 	exit 10
 }
@@ -18,7 +18,5 @@ help()
 [[ ! -e "$CADIR/ca.key" || ! -e "$CADIR/ca.crt" || ! -e "$CADIR/serial" ]] && help 
 
 days=$(grep days $CONFIG | awk -F '=' '{print $2}')
-openssl x509 -req -days $days -in "$WORKDIR/server.csr" -CA "$CADIR/ca.crt" -CAkey "$CADIR/ca.key" -CAserial "$CADIR/serial" -CAcreateserial -out "$WORKDIR/server.crt"
+openssl x509 -req -days $days -in "$WORKDIR/server.csr" -CA "$CADIR/ca.crt" -CAkey "$CADIR/ca.key" -CAserial "$CADIR/serial" -CAcreateserial -out "$WORKDIR/certificate.crt"
 
-# concat server + ca to bundle pem
-cat $WORKDIR/server.crt $CADIR/ca.crt >> $WORKDIR/server-bundle.pem
